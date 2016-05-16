75bc4a89562d6a0057d2d82b88295ca916323d7c
jasminish <jasmineang@hotmail.com>
2016-05-16 19:35:15 +0800

    added login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
+
+  @current_user = @current_user || User.find_by(id: session[:user_id])
+
   def new
   end
 
   def create
+    user = User.find_by(name: params[:session][:name].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path
+      # Log the user in and redirect to the homepage.
+    else
+      # Create an error message.
+      render 'new'
+    end
   end
 
   def destroy
-  end 
+  end
 end
