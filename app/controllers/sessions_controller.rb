1717afa5f2adc4d65e67f80dde864946d05b9551
jasminish <jasmineang@hotmail.com>
2016-05-19 15:41:59 +0800

    login cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:session][:name].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to root_path
       # Log the user in and redirect to the homepage.
     else
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in? #for error checking
     redirect_to root_path
   end
 end
