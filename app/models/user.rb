d57baeff82435ea0ae8d44343f7b8e80c01956d7
jasminish <jasmineang@hotmail.com>
2016-06-30 20:06:15 +0800

    updated signup view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessor :remember_token
 
   has_secure_password
+  validates :name, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 8}
 
   # Returns a random token.
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-
-
+  get 'signup' => 'users#new'
+  post 'signup' => 'users#create'
+  
   resources :users
   get 'welcome/index'
 
